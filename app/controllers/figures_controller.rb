5b4f2d86b457b7617d4a7ccb8daf59faff2076b4
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-02 14:54:42 +0000

    All figure/views built + controller actions. controller/figures_controller_spec PASSING.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,48 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    erb :'figures/index'
+  end
+
   get '/figures/new' do
     erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+      @figure = Figure.find(params[:id])
+      erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params["figure"])
+    if !params["landmark"].empty?
+      @figure.landmarks << Landmark.find_or_create_by(name: params["landmark"]["name"])
+    end
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params["title"]["name"].empty? && !params["landmark"]["name"].empty?
+      @figure.titles << @title = Title.create(params["title"])
+      @figure.landmarks << @landmark = Landmark.create(params["landmark"])
+
+    elsif !params["title"]["name"].empty?
+      @figure.titles << @title = Title.create(params["title"])
+    elsif !params["landmark"]["name"].empty?
+      @figure.landmarks << @landmark = Landmark.create(params["landmark"])
+    end
+
+    @figure.save
+     redirect to "/figures/#{@figure.id}"
   end
 
 end
