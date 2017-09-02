66896c965a995c342e2881a4c6657b933d77e6d9
Nicholas D'Amico <nickalan82@icloud.com>
2017-09-02 02:47:55 +0000

    Rspec tests: figures_controller_spec all PASSING

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.update(params["landmark"])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    Landmark.create(params["landmark"])
+    redirect to '/landmarks'
+  end
 end
